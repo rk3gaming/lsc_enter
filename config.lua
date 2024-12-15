ffabd9d09717aa30d067b67a9082557c6a4bf2f7
rk3gaming <rkfrmda3@gmail.com>
2024-12-15 16:43:26 -0600

    Added Support for Shells

--- config.lua
+++ config.lua
@@ -13,3 +13,22 @@ Config.Doors = {
         radius = 2.0, 
     },
 }
+
+-- Coords = Door Location
+-- EnterCoords = Where in the shell/interior they spawn in at
+-- ExitCoords = where in the interior they can use /exit at
+-- ShellModel = The Name of the shell its spawning.
+-- ShellCooods = Where the shell will spawn at, I personally put these high up in the sky.
+-- ShellHeading = Shell Heading..
+-- Radius = How close they have to be at the door to use /enter & /exit
+
+Config.Shells = {
+    {
+        coords = vector3(464.29, -1852.11, 27.79), 
+        enterCoords = vector3(2462.19, -3745.72, 87.58), 
+        exitCoords = vector3(2458.87, -3743.85, 87.58),
+        shellModel = 'shell_store3',
+        shellCoords = vector3(2464.85, -3748.81, 85.57),
+        shellHeading = 229.47,
+        radius = 2.0,
+    },
