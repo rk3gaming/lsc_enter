fb8f9d52d4712ebf641e162069ba4114ab73937b
rk3gaming <rkfrmda3@gmail.com>
2024-12-23 04:20:47 -0600

    Update

--- client/cl_customdoors.lua
+++ client/cl_customdoors.lua
@@ -1,14 +1,24 @@
-Config = Config or {}
-Config.Doors = Config.Doors or {}
-Config.Shells = Config.Shells or {}
-
-local lastMessageTime = 0
-local activeSession = nil 
+local lastEnterMessageTime = 0
+local lastExitMessageTime = 0
 local spawnedShells = {}
+local show3DText = false
+local isNearExit = false
+
+function Draw3DTextFixed(screenX, screenY, text)
+    SetTextFont(0)
+    SetTextProportional(0)
+    SetTextScale(0.45, 0.45)
+    SetTextOutline() 
+    SetTextCentre(1) 
+
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(screenX, screenY)
+end
 
 Citizen.CreateThread(function()
     for _, shell in ipairs(Config.Shells) do
-        if shell.shellModel and not spawnedShells[shell.coords] then
+        if shell.shellModel and not spawnedShells[shell.shellCoords] then
             RequestModel(GetHashKey(shell.shellModel))
             while not HasModelLoaded(GetHashKey(shell.shellModel)) do
                 Citizen.Wait(10)
@@ -17,36 +27,60 @@ Citizen.CreateThread(function()
             local shellObject = CreateObject(GetHashKey(shell.shellModel), shell.shellCoords.x, shell.shellCoords.y, shell.shellCoords.z, false, false, false)
             SetEntityHeading(shellObject, shell.shellHeading)
             FreezeEntityPosition(shellObject, true)
-            spawnedShells[shell.coords] = shellObject
+            spawnedShells[shell.shellCoords] = shellObject
         end
     end
 
     while true do
-        Citizen.Wait(1000)
+        Citizen.Wait(0)
 
         local playerPed = PlayerPedId()
         local playerCoords = GetEntityCoords(playerPed)
-        local isNearDoor = false
+        show3DText = false
+        isNearExit = false
 
-        for _, item in ipairs(Config.Doors) do
-            if #(playerCoords - item.coords) <= item.radius then
-                isNearDoor = true
-                if GetGameTimer() - lastMessageTime >= 30000 then
-                    TriggerEvent('chatMessage', '', {255, 255, 255}, '^4[INFO]: ^0You are near a door. Use ^3/enter^7 to enter this interior.')
-                    lastMessageTime = GetGameTimer()
+        for _, door in ipairs(Config.Doors) do
+            if #(playerCoords - door.coords) <= door.radius then
+                show3DText = true
+                if GetGameTimer() - lastEnterMessageTime >= 30000 then 
+                    TriggerEvent('chatMessage', '', {255, 255, 255}, '^4[INFO]: ^0Use ^3/enter^0 to enter this interior.')
+                    lastEnterMessageTime = GetGameTimer() 
                 end
                 break
             end
         end
 
-        for _, shell in ipairs(Config.Shells) do
-            if #(playerCoords - shell.coords) <= shell.radius then
-                isNearDoor = true
-                if GetGameTimer() - lastMessageTime >= 30000 then
-                    TriggerEvent('chatMessage', '', {255, 255, 255},'^4[INFO]: ^0You are near a door. Use ^3/enter^7 to enter this interior.')
-                    lastMessageTime = GetGameTimer()
+        for _, door in ipairs(Config.Doors) do
+            if #(playerCoords - door.exitCoords) <= door.radius then
+                show3DText = true
+                isNearExit = true
+                if GetGameTimer() - lastExitMessageTime >= 30000 then 
+                    TriggerEvent('chatMessage', '', {255, 255, 255}, '^4[INFO]: ^0Use ^3/exit^0 to exit this interior.')
+                    lastExitMessageTime = GetGameTimer() 
                 end
                 break
+            end
+        end
+
+        if show3DText then
+            if isNearExit then
+                Draw3DTextFixed(0.5, 0.9, "~w~Press ~y~Y ~w~to exit this interior")
+            else
+                Draw3DTextFixed(0.5, 0.9, "~w~Press ~y~Y ~w~to enter this interior")
+            end
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        
+        if IsControlJustPressed(0, 246) then  
+            if isNearExit then
+                ExecuteCommand("exit")
+            else
+                ExecuteCommand("enter")
             end
         end
     end
@@ -58,18 +92,32 @@ RegisterCommand("enter", function()
 
     for _, door in ipairs(Config.Doors) do
         if #(playerCoords - door.coords) <= door.radius then
-            SetEntityCoords(playerPed, door.enterCoords.x, door.enterCoords.y, door.enterCoords.z, false, false, false, true)
-            TriggerServerEvent('customdoors:setRoutingBucket', door.coords)
-            activeSession = { type = "door", coords = door.coords }
+            local destination = vector3(door.enterCoords.x, door.enterCoords.y, door.enterCoords.z)
+            if destination then
+                SetEntityCoords(playerPed, destination.x, destination.y, destination.z, false, false, false, true)
+                SetEntityHeading(playerPed, door.enterHeading) 
+                TriggerServerEvent('customdoors:setRoutingBucket', door.coords)
+
+                TriggerEvent('av_weather:freeze', true, 12, 0, "CLEAR", false, "normal", false)
+            end
             return
         end
     end
 
     for _, shell in ipairs(Config.Shells) do
-        if #(playerCoords - shell.coords) <= shell.radius then
-            SetEntityCoords(playerPed, shell.enterCoords.x, shell.enterCoords.y, shell.enterCoords.z, false, false, false, true)
-            TriggerServerEvent('customdoors:setRoutingBucket', shell.coords)
-            activeSession = { type = "shell", coords = shell.coords }
+        if #(playerCoords - shell.shellCoords) <= 2.0 then 
+            local shellModelHash = GetHashKey(shell.shellModel)
+            if not DoesEntityExist(spawnedShells[shell.shellCoords]) then
+                RequestModel(shellModelHash)
+                while not HasModelLoaded(shellModelHash) do
+                    Citizen.Wait(10)
+                end
+
+                local shellObject = CreateObject(shellModelHash, shell.shellCoords.x, shell.shellCoords.y, shell.shellCoords.z, false, false, false)
+                SetEntityHeading(shellObject, shell.shellHeading)
+                FreezeEntityPosition(shellObject, true)
+                spawnedShells[shell.shellCoords] = shellObject
+            end
             return
         end
     end
@@ -78,35 +126,19 @@ RegisterCommand("enter", function()
 end, false)
 
 RegisterCommand("exit", function()
-    if not activeSession then
-        TriggerEvent('chatMessage', '', {255, 255, 255}, '^1[ERROR]: ^0You are not in any interior.')
-        return
-    end
-
     local playerPed = PlayerPedId()
-    local exitCoords = nil
+    local playerCoords = GetEntityCoords(playerPed)
 
-    if activeSession.type == "door" then
-        for _, door in ipairs(Config.Doors) do
-            if door.coords == activeSession.coords then
-                exitCoords = door.coords
-                break
-            end
-        end
-    elseif activeSession.type == "shell" then
-        for _, shell in ipairs(Config.Shells) do
-            if shell.coords == activeSession.coords then
-                exitCoords = shell.coords
-                break
-            end
+    for _, door in ipairs(Config.Doors) do
+        if #(playerCoords - door.exitCoords) <= door.radius then
+            local destination = vector3(door.coords.x, door.coords.y, door.coords.z)
+            SetEntityCoords(playerPed, destination.x, destination.y, destination.z, false, false, false, true)
+
+            TriggerEvent('av_weather:freeze', false, 0, 0, "CLEAR", false, "normal", false)
+            
+            return
         end
     end
 
-    if exitCoords then
-        SetEntityCoords(playerPed, exitCoords.x, exitCoords.y, exitCoords.z, false, false, false, true)
-        TriggerServerEvent('customdoors:resetRoutingBucket')
-        activeSession = nil
-    else
-        TriggerEvent('chatMessage', '', {255, 255, 255}, '^1[ERROR]: ^0No exit point found.')
-    end
+    TriggerEvent('chatMessage', '', {255, 255, 255}, '^1[ERROR]: ^0You are not near any exit point.')
 end, false)
