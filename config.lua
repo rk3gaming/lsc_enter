1b2347c2de104dafdbe28e93c5fc905d435832f1
RK <rkfrmda3@gmail.com>
2025-01-27 15:36:41 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,25 @@
 Config = {}
 
-Config.Locations = { 
+-- coords = Door Location
+-- enterCoords = The interior where they get sent to
+-- exitCoords = Where in the interior they can use /exit at to leave
+-- radius = How close they have to be at the door to use /enter & /exit
+
+Config.Doors = {
     {
-        coords = vector3(464.08, -1852.09, 27.79), -- coords at which the "Press Y to enter" will appear at.
-        interiorcords = vector4(150.1107, -1038.1862, 29.3676, 135.6825),  -- the coords where you will teleport to after pressing Y
-        exitcoords = vector3(150.8922, -1038.1299, 29.3676), -- the coords you can "Press Y to exit" at.
-        doorId = 1 -- Door IDs (New change) meant for routing buckets, must be a different number for each door.
+        coords = vector3(-638.38, -1249.58, 11.81), 
+        enterCoords = vector3(305.36, -893.66, 835.86), 
+        enterHeading = 284.91,
+        exitCoords = vector3(297.62, -889.9, 835.86),
+        radius = 2.0,
     },
 }
+
+Config.Shells = {
+    { 
+        shellModel = 'shell_lester',
+        shellCoords = vector3(303.6, -891.48, 834.75),
+        shellHeading = 272.03,
+    },
+}
+
